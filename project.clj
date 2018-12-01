70fcb199f195ec07088cdb3fca274d44447ebd62
Andrew Cowper <andrew.cowper@slothrop.net>
2018-12-01 10:29:08 +0000

    Day 1
    Upgrade to clojure 10RC2

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject aoc2018 "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0-RC2"]]
   :plugins [[cider/cider-nrepl "0.18.0"]])
--- src/net/slothrop/aoc2018/day1.clj
+++ src/net/slothrop/aoc2018/day1.clj
@@ -1,1 +1,17 @@
-(ns net.slothrop.aoc2018.day1)
+(ns net.slothrop.aoc2018.day1
+  (:require [net.slothrop.aoc2018.input :as i]))
+
+(defn part2 [i]
+  (reduce (fn [[seen total] c]
+            (let [new-total (+ total c)]
+              (if (contains? seen new-total)
+                (reduced new-total)
+                [(conj seen new-total) new-total]))) [#{} 0] (cycle i)))
+
+(comment
+  ;; part1
+  (reduce + 0 i/day1-input)
+
+  ;;part2
+  (part2 i/day1-input)
+)
