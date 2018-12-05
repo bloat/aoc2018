9c7c5c745e16288fe4a07a3523f72126cfb5dff5
Andrew Cowper <andrew.cowper@slothrop.net>
2018-12-05 06:31:15 +0000

    day 5 - skeleton

---
+++ src/net/slothrop/aoc2018/day5.clj
@@ -0,0 +1,15 @@
+(ns net.slothrop.aoc2018.day5
+  (:require [net.slothrop.aoc2018.input :as i]
+            [clojure.java.io :as io]))
+
+(def input (slurp (io/resource "day5.txt")) "\n")
+(def test-input "dabAcCaCBAcCcaDA")
+
+
+(comment
+  ;; part 1
+
+
+  ;; part 2
+
+  )
