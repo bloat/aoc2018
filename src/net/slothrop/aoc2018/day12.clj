83acda0c7bc55ba61a221159aed62d2d1fdd10c2
Andrew Cowper <andrew.cowper@slothrop.net>
2018-12-13 12:50:35 +0000

    day 12 part 2 - score function

--- src/net/slothrop/aoc2018/day12.clj
+++ src/net/slothrop/aoc2018/day12.clj
@@ -23,6 +23,12 @@
 (defn evolve-row [row]
   [(evolve-lhs row) (evolve-rhs row)])
 
+
+(defn score [row]
+  (reduce (fn [a [v s]] (if (= \# v)
+                          (+ a s)
+                          a)) 0 (map vector (take 5000 (second row)) (range))))
+
 (comment
   (set! *unchecked-math* :warn-on-boxed)
   (set! *warn-on-reflection* true)
@@ -36,5 +42,7 @@
   ;; part 2
   ;; Not much extra code - found the cycle by looking at Mathematica's CellularAutomaton output. Then 
   ;; fiddled around here to get the start point of the cycle and the score increment each time
-  
+  (take 50 (drop 180 (map vector (map score (iterate evolve-row start-row)) (range))))
+  (+ 6505 (* 34 (- 50000000000 191)))
+
   )
